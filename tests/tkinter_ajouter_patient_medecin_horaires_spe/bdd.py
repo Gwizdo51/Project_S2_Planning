260809b36b35ba9eb7dd764362a565aca6ede0f8
le marchand de mort <piercieslak@gmail.com>
2024-05-12 21:16:54 +0200

    ajout des fonctions de modifications de medecin / rdv / patient / horaires_speciaux

--- tests/tkinter_ajouter_patient_medecin_horaires_spe/bdd.py
+++ tests/tkinter_ajouter_patient_medecin_horaires_spe/bdd.py
@@ -79,3 +79,38 @@ def supprimer_ref_hor_spe(ref_hor_spe):
     curseur.execute(sql_query, (ref_hor_spe,))
     connexion_mysql.commit()
     connexion_mysql.close()
+
+
+
+
+def modifier_rdv(nouvelle_heure_debut, nouvelle_duree, ref_rdv):
+    connexion_mysql = mysql.connect(user='root', password='root', host='127.0.0.1', database='doctime', port=3306)
+    curseur = connexion_mysql.cursor()
+    sql_query = "UPDATE rdv SET date_heure_debut = %s, duree = %s WHERE ref_patient = %s"
+    curseur.execute(sql_query, (nouvelle_heure_debut, nouvelle_duree, ref_rdv))
+    connexion_mysql.commit()
+    connexion_mysql.close()
+
+def modifier_patient(nouveau_nom, nouveau_prenom, nouveau_num_tel , nouvelle_civilite , ref_patient):
+    connexion_mysql = mysql.connect(user='root', password='root', host='127.0.0.1', database='doctime', port=3306)
+    curseur = connexion_mysql.cursor()
+    sql_query = "UPDATE patient SET nom = %s, prenom = %s , num_tel %s , civilite = %s WHERE ref_patient = %s"
+    curseur.execute(sql_query, (nouveau_nom, nouveau_prenom,nouveau_num_tel,nouvelle_civilite, ref_patient))
+    connexion_mysql.commit()
+    connexion_mysql.close()
+
+def modifier_medecin(nv_nom, nv_prenom,nv_num_tel,nv_specialite,nv_horaire_lundi,nv_horaire_mardi,nv_horaire_mercredi,nv_horaire_jeudi,nv_horaire_vendredi,nv_horaire_samedi,nv_horaire_dimanche,ref_medecin):
+    connexion_mysql = mysql.connect(user='root', password='root', host='127.0.0.1', database='doctime', port=3306)
+    curseur = connexion_mysql.cursor()
+    sql_query = "UPDATE medecin SET nom = %s,prenom = %s ,num_tel = %s ,specialite = %s ,horaires_lundi = %s ,horaires_mardi = %s ,horaires_mercredi = %s ,horaires_jeudi = %s ,horaires_vendredi = %s,horaires_samedi = %s,horaires_dimanche = %s WHERE ref_patient = %s"
+    curseur.execute(sql_query, (nv_nom, nv_prenom,nv_num_tel,nv_specialite,nv_horaire_lundi,nv_horaire_mardi,nv_horaire_mercredi,nv_horaire_jeudi,nv_horaire_vendredi,nv_horaire_samedi,nv_horaire_dimanche,ref_medecin))
+    connexion_mysql.commit()
+    connexion_mysql.close()
+
+def modifier_horaires_speciaux(nv_dt_db,nv_dt_fin,nv_h_lundi,nv_h_mardi,nv_h_mercredi,nv_h_jeudi,nv_h_vendredi,nv_h_samedi,nv_h_dimanche,ref_medecin,ref_hor_spe):
+    connexion_mysql = mysql.connect(user='root', password='root', host='127.0.0.1', database='doctime', port=3306)
+    curseur = connexion_mysql.cursor()
+    sql_query = "UPDATE horaires_speciaux SET date_debut = %s,date_fin = %s, horaires_lundi = %s ,horaires_mardi = %s ,horaires_mercredi = %s ,horaires_jeudi = %s ,horaires_vendredi = %s,horaires_samedi = %s,horaires_dimanche = %s, ref_medecin = %s WHERE ref_hor_spe = %s"
+    curseur.execute(sql_query, (nv_dt_db,nv_dt_fin,nv_h_lundi,nv_h_mardi,nv_h_mercredi,nv_h_jeudi,nv_h_vendredi,nv_h_samedi,nv_h_dimanche,ref_medecin,ref_hor_spe))
+    connexion_mysql.commit()
+    connexion_mysql.close()
